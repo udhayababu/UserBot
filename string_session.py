c06638e3fbddef0362141f8ded381a3eb7473c04
udhayababu <udhayababu09@gmail.com>
2019-11-13 16:38:38 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -7,5 +7,5 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
 
-with TelegramClient(StringSession(), 1140463, 7c8b92de6ff611a99a3acdecac2fb87a) as client:
+with TelegramClient(StringSession(), "1140463", "7c8b92de6ff611a99a3acdecac2fb87a") as client:
     print(client.session.save())
